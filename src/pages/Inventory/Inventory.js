7277296ef4f16705f6f53b0a3a3c3bb6fd48b362
Ashish Maharjan <amaharjan1032@gmail.com>
2020-11-01 16:40:31 +0545

    API fetch

--- src/pages/Inventory/Inventory.js
+++ src/pages/Inventory/Inventory.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { Container, Row, Table, Button } from "react-bootstrap";
 import styled from "styled-components";
 
@@ -17,35 +18,47 @@ const TD2 = styled.td`
 `;
 
 function Inventory() {
+  const [Category, setCategory] = useState("");
+  const [Response, setResponse] = useState([]);
+
+  useEffect(() => {
+    const apiUrl = `http://www.nasatechnicalsolutions.com.np/api/products`;
+    axios
+      .get(apiUrl)
+      .then((response) => {
+        setResponse(response.data.data);
+        console.log(response.data.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  // selectCategory = () => {
+  //   const total = Response.length;
+  //   console.log(total);
+  //   category = Response.map((e) => e.category);
+  //   console.log(category);
+  // };
+
   return (
     <Container>
       <Row className="my-3">
         <h2>Categories</h2>
-        <Button variant="outline-info" className="mx-4">
-          All
-        </Button>
-        <Button variant="outline-info" className="mx-4" active>
-          Liquor
-        </Button>
-        <Button variant="outline-info" className="mx-4">
-          Meat
-        </Button>
-        <Button variant="outline-info" className="mx-4">
-          Fruits
-        </Button>
-        <Button variant="outline-info" className="mx-4">
-          Cosmetic
-        </Button>
-        <Button variant="outline-info" className="mx-4">
-          Grocery
-        </Button>
-        <Button variant="outline-info" className="mx-4">
-          Cakes
-        </Button>
+        {Response.slice(0, 6).map((cat) => (
+          <Button
+            variant="outline-info"
+            className="mx-4"
+            onClick={(e) => setCategory(cat.category)}
+            key={cat.id}
+          >
+            {cat.category}
+          </Button>
+        ))}
         <Button
           variant="primary"
           size="sm"
-          className="ml-4"
+          className="ml-2"
           style={{ fontSize: "17px" }}
         >
           Add Products
@@ -72,108 +85,27 @@ function Inventory() {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <TD1>1</TD1>
-            <TD>Tuborg Beer</TD>
-            <TD>20</TD>
-            <TD>350</TD>
-            <TD>5%</TD>
-            <TD>Lorem ipsum dolor sit amet</TD>
-            <TD>asd7f3434h</TD>
-            <TD2>
-              <Button variant="success" size="sm" className="mr-2">
-                Edit
-              </Button>
-              <Button variant="danger" size="sm">
-                Delete
-              </Button>
-            </TD2>
-          </tr>
-          <tr>
-            <TD1>1</TD1>
-            <TD>Tuborg Beer</TD>
-            <TD>20</TD>
-            <TD>350</TD>
-            <TD>5%</TD>
-            <TD>Lorem ipsum dolor sit amet</TD>
-            <TD>asd7f3434h</TD>
-            <TD2>
-              <Button variant="success" size="sm" className="mr-2">
-                Edit
-              </Button>
-              <Button variant="danger" size="sm">
-                Delete
-              </Button>
-            </TD2>
-          </tr>
-          <tr>
-            <TD1>1</TD1>
-            <TD>Tuborg Beer</TD>
-            <TD>20</TD>
-            <TD>350</TD>
-            <TD>5%</TD>
-            <TD>Lorem ipsum dolor sit amet</TD>
-            <TD>asd7f3434h</TD>
-            <TD2>
-              <Button variant="success" size="sm" className="mr-2">
-                Edit
-              </Button>
-              <Button variant="danger" size="sm">
-                Delete
-              </Button>
-            </TD2>
-          </tr>
-          <tr>
-            <TD1>1</TD1>
-            <TD>Tuborg Beer</TD>
-            <TD>20</TD>
-            <TD>350</TD>
-            <TD>5%</TD>
-            <TD>Lorem ipsum dolor sit amet</TD>
-            <TD>asd7f3434h</TD>
-            <TD2>
-              <Button variant="success" size="sm" className="mr-2">
-                Edit
-              </Button>
-              <Button variant="danger" size="sm">
-                Delete
-              </Button>
-            </TD2>
-          </tr>
-          <tr>
-            <TD1>1</TD1>
-            <TD>Tuborg Beer</TD>
-            <TD>20</TD>
-            <TD>350</TD>
-            <TD>5%</TD>
-            <TD>Lorem ipsum dolor sit amet</TD>
-            <TD>asd7f3434h</TD>
-            <TD2>
-              <Button variant="success" size="sm" className="mr-2">
-                Edit
-              </Button>
-              <Button variant="danger" size="sm">
-                Delete
-              </Button>
-            </TD2>
-          </tr>
-          <tr>
-            <TD1>1</TD1>
-            <TD>Tuborg Beer</TD>
-            <TD>20</TD>
-            <TD>350</TD>
-            <TD>5%</TD>
-            <TD>Lorem ipsum dolor sit amet</TD>
-            <TD>asd7f3434h</TD>
-            <TD2>
-              <Button variant="success" size="sm" className="mr-2">
-                Edit
-              </Button>
-              <Button variant="danger" size="sm">
-                Delete
-              </Button>
-            </TD2>
-          </tr>
+          {Response.filter((data) => data.category === Category).map(
+            (data, index) => (
+              <tr key={data.id}>
+                <TD1>{index + 1}</TD1>
+                <TD>{data.productName}</TD>
+                <TD>{data.stock}</TD>
+                <TD>{data.price}</TD>
+                <TD>{data.discountRate}%</TD>
+                <TD>{data.description}</TD>
+                <TD>eqwe672326</TD>
+                <TD2>
+                  <Button variant="success" size="sm" className="mr-2">
+                    Edit
+                  </Button>
+                  <Button variant="danger" size="sm">
+                    Delete
+                  </Button>
+                </TD2>
+              </tr>
+            )
+          )}
         </tbody>
       </Table>
     </Container>
